d48534f68e233d6f6a00f16f24918d17b401019f
Sailesh Patra <saileshpatra997@gmail.com>
2021-08-30 02:02:19 +0530

    Navigating to a route progarmatically(not by clicking on HTML link elements)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { } // To navigate we need to get access to our router. Injecting the angular router in this component to get the same instance of router in which we registered our routes.
+
 
   ngOnInit() {
   }
 
+  onLoadServers(){
+    this.router.navigate(["/servers"]);
+    // The parameter that navigate method takes is an array of element or elements depending on the path you want to get navigated to.
+    // Just like we used in routerLink in a(HTML link) elements.
+  }
 }
+
+/**
+ * In app-module that array of routes was of type, Routes
+ * 
+ * Here we're using Router. 
+ */
